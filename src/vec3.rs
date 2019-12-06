0b972e131ca517b44c12d40787f0e1b413d79e99
Spencer Elliott <spencer.elliott@xe.com>
2019-12-06 14:34:39 -0500

    Added new operators for Vector3

--- src/vec3.rs
+++ src/vec3.rs
@@ -6,12 +6,34 @@ pub struct Vector3 {
     pub z: f32
 }
 
-impl ops::Add<Vector3> for Vector3 {
+impl ops::Add for Vector3 {
     type Output = Vector3;
 
     fn add(self, _rhs: Vector3) -> Vector3 {
-        let ret = Vector3 { x: self.x + _rhs.x,  y: self.y + _rhs.y, z: self.z + _rhs.z };
+        Vector3 { x: self.x + _rhs.x,  y: self.y + _rhs.y, z: self.z + _rhs.z }
+    }
+}
 
-        return ret;
+impl ops::Sub for Vector3 {
+    type Output = Vector3;
+
+    fn sub(self, _rhs: Vector3) -> Vector3 {
+        Vector3 { x: self.x - _rhs.x, y: self.y - _rhs.y, z: self.z - _rhs.z }
+    }
+}
+
+impl ops::Mul for Vector3 {
+    type Output = Vector3;
+
+    fn mul(self, _rhs: Vector3) -> Vector3 {
+        Vector3 { x: self.x * _rhs.x, y: self.y * _rhs.y, z: self.z * _rhs.z }
+    }
+}
+
+impl ops::Div for Vector3 {
+    type Output = Vector3;
+
+    fn div(self, _rhs: Vector3) -> Vector3 {
+        Vector3 { x: self.x / _rhs.x, y: self.y / _rhs.y, z: self.z / _rhs.z }
     }
 }
